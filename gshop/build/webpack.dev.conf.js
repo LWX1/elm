dcb22552e0d7f3d88c791a7f100ba009281d157e
LWX1 <48095854+LWX1@users.noreply.github.com>
2021-02-17 23:05:05 +0800

    test-添加header

--- gshop/build/webpack.dev.conf.js
+++ gshop/build/webpack.dev.conf.js
@@ -23,9 +23,9 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
-    // headers: {
-    //   'Access-Control-Allow-Origin': '*'
-    // },
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     disableHostCheck: true, 
     clientLogLevel: 'warning',
     historyApiFallback: {
