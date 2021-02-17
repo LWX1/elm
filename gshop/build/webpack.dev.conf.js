364c17731f5bd105d223cb0255e3f9ecdbe79db2
LWX1 <48095854+LWX1@users.noreply.github.com>
2021-02-17 22:44:42 +0800

    test

--- gshop/build/webpack.dev.conf.js
+++ gshop/build/webpack.dev.conf.js
@@ -22,13 +22,13 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    disableHostCheck: true, 
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
         { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
       ],
     },
-    disableHostCheck: true, 
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
--- gshop/config/index.js
+++ gshop/config/index.js
@@ -20,7 +20,7 @@ module.exports = {
 
       }
     },
-
+    disableHostCheck: true, 
     // Various Dev Server settings
     host: '127.0.0.1', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
